a9a679972539f1b2081b1effe941c84c16da9805
HingusPingus <alumno26.lopez.calo.salvador@ipm.edu.ar>
2026-04-09 16:15:00 -0300

    fondo hecho y problemas con animación de salto

--- scenes/character.gd
+++ scenes/character.gd
@@ -3,24 +3,39 @@ extends CharacterBody2D
 @onready var _animated_sprite = $AnimatedSprite2D
 var speed=300
 const JUMP_VELOCITY = -400.0
+var jumping
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumping=true;
+
+		
+		if velocity.y>0:
+			_animated_sprite.play("fall")
+		else:
+			_animated_sprite.play("jump")
 	var direction := Input.get_axis("ui_left","ui_right")
+	if is_on_floor():
+		jumping=false;
+
 	if direction:
-		_animated_sprite.play("run")
+		if !jumping:
+			_animated_sprite.play("run")
 		_animated_sprite.flip_h = direction < 0
 
 			
 		velocity.x=direction*speed
 	else:
-		_animated_sprite.play("idle")
+		if is_on_floor():
+			_animated_sprite.play("idle")
+			
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+		
 	move_and_slide()
