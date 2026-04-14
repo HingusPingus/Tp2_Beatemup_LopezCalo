d6e269ffd970def8130ae7d319d487d5b7a52796
HingusPingus <alumno26.lopez.calo.salvador@ipm.edu.ar>
2026-04-14 10:00:54 -0300

    testeado golpes

---
+++ scenes/bicho.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+class_name Enemy
+
+var health =200
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if health<=0:
+		queue_free()
+
+func take_damage(damage):
+	health -=damage
+	print(damage)
--- scenes/character.gd
+++ scenes/character.gd
@@ -5,16 +5,20 @@ extends CharacterBody2D
 var speed=300
 const JUMP_VELOCITY = -400.0
 var jumping
+var hit_cd=0.25
 var can_attack=true
 var hitting=false
 var timer
+var damage=50
+var lastAxis=1
+var yPosition:float
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if position.y>yPosition:
 		velocity += get_gravity() * delta
 		
 	if Input.is_action_pressed("melee_attack") and can_attack:
-		timer=get_tree().create_timer(0.3)
+		timer=get_tree().create_timer(hit_cd)
 		timer.timeout.connect(animation_cd)
 		hitting=true
 		if !jumping:
@@ -22,11 +26,11 @@ func _physics_process(delta: float) -> void:
 		else:
 			_animated_sprite.play("jump-punch")
 		can_attack = false
-		timer=get_tree().create_timer(0.5)
+		timer=get_tree().create_timer(hit_cd)
 		timer.timeout.connect(cd_reset)
 		for i in hitbox.get_overlapping_bodies():
-			if i.is_in_group("Enemies"):
-				i.take_damage(20)
+			if i is Enemy:
+				i.take_damage(damage)
 	# Handle jump.
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -35,20 +39,29 @@ func _physics_process(delta: float) -> void:
 	elif is_on_floor():
 		jumping=false;
 		
+	if Input.is_action_just_pressed("ui_left") and lastAxis==1:
+		scale.x=-1
+		lastAxis=-1
+		
+	if Input.is_action_just_pressed("ui_right") and lastAxis==-1:
+		scale.x=-1
+		lastAxis=1
+		
 	elif velocity.y>0 and jumping and !hitting:
 		_animated_sprite.play("fall")
 	elif jumping and !hitting:
 		_animated_sprite.play("jump")
+	var directiony := Input.get_axis("ui_up", "ui_down")
+	var directionx := Input.get_axis("ui_left","ui_right")
 	
-	var direction := Input.get_axis("ui_left","ui_right")
-
-	if direction and (!hitting or jumping):
+	if directiony and(!hitting):
+		velocity.y=directiony*speed
+		yPosition=position.y
+	
+	if directionx and (!hitting or jumping):
 		if !jumping:
 			_animated_sprite.play("run")
-		_animated_sprite.flip_h = direction < 0
-
-			
-		velocity.x=direction*speed
+		velocity.x=directionx*speed
 	else:
 		if is_on_floor() and !hitting:
 			_animated_sprite.play("idle")
